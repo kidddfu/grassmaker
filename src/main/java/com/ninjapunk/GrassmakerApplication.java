d32ce24ca86c85a65c1da31228bba795b5b9de30
liwb <1525809014@qq.com>
2017-10-17 14:16:44 +0800

    创建项目

---
+++ src/main/java/com/ninjapunk/GrassmakerApplication.java
@@ -0,0 +1,12 @@
+package com.ninjapunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrassmakerApplication.class, args);
+	}
+}
